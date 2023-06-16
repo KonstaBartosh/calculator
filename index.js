0655d035d821e5ea7eda51d22ac53658588555db
Konstantin Bartosh <rustigillo@gmail.com>
2023-06-16 14:24:02 +0600

    Feat: add logic to displaying basic math operations (! Works only once correctlby)

--- index.js
+++ index.js
@@ -1,46 +1,70 @@
-const plusButton = document.querySelector('.operator_plus');
-const minusButton = document.querySelector('.operator_minus');
-const multiplyButton = document.querySelector('.operator_mult');
-const divideButton = document.querySelector('.operator_divide');
-const equal = document.querySelector('.equals');
+const plusButton = document.querySelector(".operator_plus");
+const minusButton = document.querySelector(".operator_minus");
+const multiplyButton = document.querySelector(".operator_multiplying");
+const divideButton = document.querySelector(".operator_divide");
+const equal = document.querySelector(".equals");
+const display = document.querySelector(".display");
+const clear = document.querySelector(".clear");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
 
-const a = 2;
-const b = 5;
-const operator = '';
+let a = "";
+let b = "";
+let operand = "";
+let isOperatorClicked = false;
 
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    let buttonNumber = number.textContent;
 
-const addNumbers = (a, b) => {
-	return a + b;
-}
+    if (isOperatorClicked) {
+      display.textContent += buttonNumber; // concatenating numbers
+      b = parseInt(b + buttonNumber); // convert the concatenated string to a number
+    } else {
+      display.textContent += buttonNumber;
+      a = parseInt(a + buttonNumber);
+    }
+  });
+});
 
-const subtractNumbers = (a, b) => {
-	return a - b;
-}
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    isOperatorClicked = true;
+    let result = operator.textContent;
+    display.textContent = result;
+    operand = result;
+  });
+});
 
-const multiplyNumbers = (a, b) => {
-	return a * b;
-}
+//* basic math operators */
+const addNumbers = (a, b) => a + b;
+const subtractNumbers = (a, b) => a - b;
+const multiplyNumbers = (a, b) => a * b;
+const divideNumbers = (a, b) => a / b;
 
-const divideNumbers = (a, b) => {
-	return a / b;
-}
+const operate = (operand, a, b) => {
+  switch (operand) {
+    case "+":
+      return addNumbers(a, b);
+    case "-":
+      return subtractNumbers(a, b);
+    case "x":
+      return multiplyNumbers(a, b);
+    case "÷":
+      return divideNumbers(a, b);
+  }
+};
 
-const operate = (operator, a, b) => {
-	switch (operator) {
-		case plusButton:
-			return addNumbers(a, b);
-			break;
-		case minusButton:
-			return subtractNumbers(a, b);
-			break;
-		case multiplyButton:
-			return multiplyNumbers(a, b);
-			break;
-		case divideButton:
-			return divideNumbers(a ,b);
-			break;
-	}
-}
+clear.addEventListener("click", () => {
+  display.textContent = "";
+  a = "";
+  b = "";
+  operand = "";
+});
 
-
-console.log(operate(minusButton, 6, 6));
+equal.addEventListener("click", () => {
+  display.textContent = operate(operand, a, b);
+  a = "";
+  b = "";
+  operand = "";
+});
